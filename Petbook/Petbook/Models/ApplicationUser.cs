9de1395b50646cba1816118ad29e8f84ece39e9c
neagamaria <maria.neagabudoiu@gmail.com>
2023-03-07 19:11:48 +0200

    Add SeedData class

--- Petbook/Petbook/Models/ApplicationUser.cs
+++ Petbook/Petbook/Models/ApplicationUser.cs
@@ -7,20 +7,16 @@ namespace Petbook.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public DateTime? JoinDate { get; set; }
+
+        public string? ProfilePhoto { get; set; }
+
         public virtual ICollection<Pet>? Pets { get; set; }
-        
+
         public virtual ICollection<Comment>? Comments { get; set; }
         public virtual ICollection<ApplicationUser>? Followers { get; set; }
 
         public virtual ICollection<BlogPost>? BlogPosts { get; set; }
-        [Required(ErrorMessage = "The username must be specified")]
-        public string Username { get; set; }
-        [Required(ErrorMessage = "The email must be specified")]
-        public string Email { get; set; }   
-
-        public  DateTime? JoinDate { get; set; }
-
-        public string? ProfilePhoto { get; set; }
 
         [NotMapped]
         public IEnumerable<SelectListItem>? AllRoles { get; set; }
