7b238223e738361a6845b786d3591a3b045c8840
neagamaria <92460510+neagamaria@users.noreply.github.com>
2023-03-11 08:07:34 +0200

    Update BlogPostLikesController.cs

--- Petbook/Petbook/Controllers/BlogPostLikesController.cs
+++ Petbook/Petbook/Controllers/BlogPostLikesController.cs
@@ -1,12 +1,71 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Petbook.Data;
+using Petbook.Models;
+using System.Data;
 
 namespace Petbook.Controllers
 {
     public class BlogPostLikesController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext db;
+
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public BlogPostLikesController(
+            ApplicationDbContext context,
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager
+            )
         {
+            db = context;
+
+            _userManager = userManager;
+
+            _roleManager = roleManager;
+        }
+
+        [Authorize(Roles = "User")]
+        public IActionResult Show(int blogPostId)
+        {
+            var blogpostlikes = from blogpostlike in db.BlogPostLikes
+                                                        .Include("User")
+                                                        .Include("BlogPost")
+                                                        .Where(bpl => bpl.BlogPostId == blogPostId)
+                                select blogpostlike;
+            ViewBag.BlogPostLikes = blogpostlikes;
+
             return View();
+        }
+
+        [Authorize(Roles = "User")]
+        public IActionResult Delete(int blogPostId)
+        {
+            var blogPostLikes = from blogPostLike in db.BlogPostLikes
+                               .Where(bpl => bpl.BlogPostId == blogPostId &&
+                               bpl.UserId == _userManager.GetUserId(User))
+                               select blogPostLike;
+            if(blogPostLikes != null)
+            {
+                db.Remove(blogPostLikes);
+            }
+            return RedirectToAction("BlogPosts", "Index");
+        }
+
+        [Authorize(Roles = "User")]
+        public IActionResult New(int blogPostId)
+        {
+            var blogPostLike = new BlogPostLike();
+            blogPostLike.UserId = _userManager.GetUserId(User);
+            blogPostLike.BlogPostId = blogPostId;
+
+            db.BlogPostLikes.Add(blogPostLike);
+
+            return RedirectToAction("BlogPosts", "Index");
         }
     }
 }
