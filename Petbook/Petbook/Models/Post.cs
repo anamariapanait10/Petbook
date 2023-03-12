2ab03b270ec43d2d54ded54f5f7fb53243f36ff2
ana <ana.panait10@yahoo.com>
2023-03-12 20:24:02 +0200

    Add new view for posts

--- Petbook/Petbook/Models/Post.cs
+++ Petbook/Petbook/Models/Post.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Petbook.Models
 {
@@ -10,10 +12,13 @@ namespace Petbook.Models
         public virtual Pet? Pet { get; set; }
 
         [Required (ErrorMessage = "Photo is required")]
-        public string? Photo { get; set; }
+        public string? PostPhoto { get; set; }
         public string? Description { get; set; }
         public DateTime? PostDate {get; set; }
         public virtual ICollection<Comment>? Comments { get; set; }
         public virtual ICollection<PostLike>? PostLikes { get; set; }
+
+        [NotMapped]
+        public IEnumerable<SelectListItem> Pets { get; set; }
     }
 }
