92d0dff4a7c542e279ffde67985ef3bcd1fac2c3
ana <ana.panait10@yahoo.com>
2023-03-12 15:23:41 +0200

    Add ApplicationUsersController

--- Petbook/Petbook/Controllers/ApplicationUsersController.cs
+++ Petbook/Petbook/Controllers/ApplicationUsersController.cs
@@ -1,12 +1,152 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Petbook.Data;
+using Petbook.Models;
 
 namespace Petbook.Controllers
 {
     public class ApplicationUsersController : Controller
     {
+        private readonly ApplicationDbContext db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public ApplicationUsersController(
+            ApplicationDbContext context,
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager
+            )
+        {
+            db = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
         public IActionResult Index()
         {
+            var users = db.Users;
+            ViewBag.UsersList = users;
             return View();
+        }
+
+        public async Task<ActionResult> Show(string id)
+        {
+            ApplicationUser user = db.Users.Find(id);
+            var roles = await _userManager.GetRolesAsync(user);
+
+            ViewBag.Roles = roles;
+
+            return View(user);
+        }
+
+        public async Task<ActionResult> Edit(string id)
+        {
+            ApplicationUser user = db.Users.Find(id);
+
+            user.AllRoles = GetAllRoles();
+
+            // list of all role names
+            var roleNames = await _userManager.GetRolesAsync(user); 
+
+            // search the role id in the database
+            var currentUserRole = _roleManager.Roles
+                                              .Where(r => roleNames.Contains(r.Name))
+                                              .Select(r => r.Id)
+                                              .First();
+            ViewBag.UserRole = currentUserRole;
+            return View(user);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Edit(string id, ApplicationUser newData, [FromForm] string newRole)
+        {
+            ApplicationUser user = db.Users.Find(id);
+
+            user.AllRoles = GetAllRoles();
+
+            if (ModelState.IsValid)
+            {
+                user.UserName = newData.UserName;
+                user.Email = newData.Email;
+                user.JoinDate = newData.JoinDate;
+                user.ProfilePhoto = newData.ProfilePhoto;
+                user.PhoneNumber = newData.PhoneNumber;
+
+                // search all roles from the db
+                var roles = db.Roles.ToList();
+
+                foreach (var role in roles)
+                {
+                    await _userManager.RemoveFromRoleAsync(user, role.Name);
+                }
+
+                var roleName = await _roleManager.FindByIdAsync(newRole);
+                await _userManager.AddToRoleAsync(user, roleName.ToString());
+
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(string id)
+        {
+            var user = db.Users
+                         .Include("Pets")
+                         .Include("Comments")
+                         .Include("BlogPosts")
+                         .Where(u => u.Id == id)
+                         .First();
+
+            // Delete user pets
+            if (user.Pets.Count > 0)
+            {
+                foreach (var pet in user.Pets)
+                {
+                    db.Pets.Remove(pet);
+                }
+            }
+            // Delete user comments
+            if (user.Comments.Count > 0)
+            {
+                foreach (var comment in user.Comments)
+                {
+                    db.Comments.Remove(comment);
+                }
+            }
+            // Delete user blog posts
+            if (user.BlogPosts.Count > 0)
+            {
+                foreach (var bp in user.BlogPosts)
+                {
+                    db.BlogPosts.Remove(bp);
+                }
+            }
+
+            db.ApplicationUsers.Remove(user);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [NonAction]
+        public IEnumerable<SelectListItem> GetAllRoles()
+        {
+            var selectList = new List<SelectListItem>();
+
+            var roles = from role in db.Roles
+                        select role;
+
+            foreach (var role in roles)
+            {
+                selectList.Add(new SelectListItem
+                {
+                    Value = role.Id.ToString(),
+                    Text = role.Name.ToString()
+                });
+            }
+            return selectList;
         }
     }
 }
