4fc5bc71128525f239769a4369bac94cc0ee24f7
neagamaria <maria.neagabudoiu@gmail.com>
2023-03-12 15:20:28 +0200

    Add the CommentsController

---
+++ Petbook/Petbook/Controllers/CommentsController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Petbook.Data;
+using Petbook.Models;
+using System.Data;
+
+namespace Petbook.Controllers
+{
+    public class CommentsController : Controller
+    {
+        private readonly ApplicationDbContext db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public CommentsController(
+            ApplicationDbContext context,
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager
+            )
+        {
+            db = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "User,Admin")]
+        public IActionResult New(Comment comment)
+        {
+
+            if (ModelState.IsValid)
+            {
+                db.Comments.Add(comment);
+                db.SaveChanges();
+                return Redirect("/Posts/Show/" + comment.PostId);
+            }
+
+            else
+            {
+                return Redirect("/Posts/Show/" + comment.PostId);
+            }
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "User,Admin")]
+        public IActionResult Delete(int commentId)
+        {
+            Comment comment = db.Comments.Find(commentId);
+            if (comment.UserId == _userManager.GetUserId(User) || User.IsInRole("Admin"))
+            {
+                db.Comments.Remove(comment);
+                db.SaveChanges();
+                return Redirect("/Posts/Show/" + comment.CommentId);
+            }
+
+            else
+            {
+                TempData["message"] = "You cannot delete a comment which isn't yours";
+                return RedirectToAction("Index", "Posts");
+            }
+        }
+
+        [Authorize(Roles = "User,Admin")]
+        public IActionResult Edit(int commentId)
+        {
+            Comment comment = db.Comments.Find(commentId);
+
+            if (comment.UserId == _userManager.GetUserId(User) || User.IsInRole("Admin"))
+            {
+                return View(comment);
+            }
+
+            else
+            {
+                TempData["message"] = "You cannot edit a comment which isn't yours";
+                return RedirectToAction("Index", "Posts");
+            }
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "User,Admin")]
+        public IActionResult Edit(int commentId, Comment requestComment)
+        {
+            Comment comment = db.Comments.Find(commentId);
+
+            if (comment.UserId == _userManager.GetUserId(User) || User.IsInRole("Admin"))
+            {
+                if (ModelState.IsValid)
+                {
+                    comment.CommentContent = requestComment.CommentContent;
+                    db.SaveChanges();
+
+                    return Redirect("/Posts/Show/" + comment.PostId);
+                }
+                else
+                {
+                    return View(requestComment);
+                }
+            }
+            else
+            {
+                TempData["message"] = "You cannot make changes on this comment.";
+                return RedirectToAction("Index", "Posts");
+            }
+        }
+    }
+}
