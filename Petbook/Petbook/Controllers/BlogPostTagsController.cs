5ca53045ea679d54293aee48932d1025a18b70d3
neagamaria <92460510+neagamaria@users.noreply.github.com>
2023-03-12 14:04:24 +0200

    Update BlogPostTagsController.cs

--- Petbook/Petbook/Controllers/BlogPostTagsController.cs
+++ Petbook/Petbook/Controllers/BlogPostTagsController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Petbook.Data;
@@ -22,63 +22,47 @@ namespace Petbook.Controllers
             _roleManager = roleManager;
         }
 
-        //adding a new blog post tag means assigning a given tag to a given blog post
+
         [HttpPost]
         public IActionResult New(BlogPostTag requestBlogPostTag)
         {
-            if (ModelState.IsValid)
+            if (db.BlogPostTags
+                .Where(bpt => bpt.BlogPostTagId == requestBlogPostTag.BlogPostTagId)
+                .Where(bpt => bpt.BlogPostId == requestBlogPostTag.BlogPostId)
+                .Where(bpt => bpt.TagId == requestBlogPostTag.TagId)
+                .Count() > 0)
             {
-                //if the tag already exists, we cannot add another one identical to it
-                if (db.BlogPostTags
-                    .Where(bpt => bpt.BlogPostTagId == requestBlogPostTag.BlogPostTagId)
-                    .Where(bpt => bpt.BlogPostId == requestBlogPostTag.BlogPostId)
-                    .Where(bpt => bpt.TagId == requestBlogPostTag.TagId)
-                    .Count() > 0)
-                {
-                    TempData["message"] = "This tag already exists for the blog post.";
-                }
-                else
-                {
-                    db.BlogPostTags.Add(requestBlogPostTag);
-                    db.SaveChanges();
-                    TempData["message"] = "The tag was assigned to the blog post.";
-                }
+                TempData["message"] = "This tag already exists for the blog post.";
             }
             else
             {
-                TempData["message"] = "Couldn't assign the tag to the blog post.";
+                db.BlogPostTags.Add(requestBlogPostTag);
+                db.SaveChanges();
+                TempData["message"] = "The tag was assigned to the blog post.";
             }
 
-            return Redirect("/BlogPosts/Show/" + requestBlogPostTag.BlogPostTagId);
+            return Redirect("/BlogPosts/Show/" + requestBlogPostTag.BlogPostId);
         }
 
-        //when a tag is deleted, we should remove the linkage with the blog post
+
         [HttpPost]
         public IActionResult Delete(BlogPostTag requestBlogPostTag)
         {
-            if (ModelState.IsValid)
+            if (db.BlogPostTags
+                .Where(bpt => bpt.BlogPostTagId == requestBlogPostTag.BlogPostTagId)
+                .Where(bpt => bpt.BlogPostId == requestBlogPostTag.BlogPostId)
+                .Where(bpt => bpt.TagId == requestBlogPostTag.TagId)
+                .Count() > 0)
             {
-
-                if (db.BlogPostTags
-                    .Where(bpt => bpt.BlogPostTagId == requestBlogPostTag.BlogPostTagId)
-                    .Where(bpt => bpt.BlogPostId == requestBlogPostTag.BlogPostId)
-                    .Where(bpt => bpt.TagId == requestBlogPostTag.TagId)
-                    .Count() > 0)
-                {
-                    db.Remove(requestBlogPostTag);
-                    db.SaveChanges();
-                }
-                else
-                {
-                    TempData["message"] = "The blog post tag doesn't exist.";
-                }
+                db.Remove(requestBlogPostTag);
+                db.SaveChanges();
             }
             else
             {
-                TempData["message"] = "Couldn't delete the blog post tag.";
+                TempData["message"] = "The blog post tag doesn't exist.";
             }
 
-            return Redirect("/BlogPosts/Index/");
+            return Redirect("/BlogPosts/Show/" + requestBlogPostTag.BlogPostId);
         }
     }
 }
